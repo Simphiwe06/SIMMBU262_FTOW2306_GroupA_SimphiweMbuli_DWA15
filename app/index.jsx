35c5a00c4011d8af5784de94f88e51521abd3615
Simphiwe06 <simphiwembuli06@gmail.com>
2023-11-13 13:43:42 +0200

    DWA15

---
+++ app/index.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+import ReactDOM from 'react-dom/client'
+import App from './src/App'
+// import Navbar from "./src/components/NavBar"
+// import './src/index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <>
+    <App />
+    </>
+)
---
+++ app/src/App.jsx
@@ -0,0 +1,20 @@
+import React from "react"
+import  ReactDOM  from "react-dom"
+import Main from "./components/Main"
+import Navbar from "./components/NavBar"
+
+function App() {
+  
+
+  return (
+    <> 
+      <div className="container">
+          <Navbar />
+          <Main />
+      </div>
+
+    </>
+  )
+}
+
+export default App
